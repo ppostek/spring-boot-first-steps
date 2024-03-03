a026a37667639911bc3cb045f569e3f0431fbb33
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2024-03-03 15:03:15 +0100

    Initial commit

---
+++ src/main/java/pl/edu/wszib/springbootfirststeps/SpringBootFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.springbootfirststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/springbootfirststeps/SpringBootFirstStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.springbootfirststeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
